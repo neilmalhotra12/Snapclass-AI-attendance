64e5ee37b1461899eab07945860a6fd231bbc117
Neil Malhotra <neilmalhotra689@gmail.com>
2026-09-04 08:53:01 +0530

    Added Add photos btn

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -7,6 +7,7 @@ from src.components.subject_card import subject_card
 from src.database.db import check_teacher_exists, create_teacher, teacher_login, get_teacher_subjects
 from src.components.dialog_create_subject  import create_subject_dialog
 from src.components.dialog_share_subject  import share_subject_dialog
+from src.components.dialog_add_photo import add_photos_dialog
 
 def teacher_screen():
 
@@ -71,7 +72,33 @@ def teacher_dashboard():
     footer_dashboard()
 
 def teacher_tab_take_attendance():
+    teacher_id = st.session_state.teacher_data["teacher_id"]
     st.header("Take AI Attendance")
+
+    if "attendance_images" not in st.session_state:
+        st.session_state.attendance_images = []
+
+    subjects = get_teacher_subjects(teacher_id)
+
+    if not subjects:
+        st.warning("You haven't created any subjects yet. Please create one to begin")
+        return 
+
+    subject_options = {f'{s["name"]} - {s["subject_code"]}':s["subject_id"]  for s in subjects}
+
+    col1, col2 = st.columns([3,1])
+    with col1:
+        selected_subject_label = st.selectbox("Select Subject", options=list(subject_options.keys()))
+
+    with col2:
+        if st.button("Add Photos", type="primary", icon=":material/photo_prints:", width="stretch"):
+            add_photos_dialog()
+
+    selected_subject_id = subject_options[selected_subject_label]
+
+    st.divider()
+
+
 
 def teacher_tab_manage_subjects():
     teacher_id = st.session_state.teacher_data["teacher_id"]
